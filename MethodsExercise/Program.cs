ee4de025fe0435920f6fa4175156c03c370618f4
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-03-05 15:24:11 -0800

    Completed the Excercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace MethodsExercise
+﻿using System.Threading.Channels;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        public static void Madlib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("Favorite band?");
+            string favBand = Console.ReadLine();
+            
+            Console.WriteLine($"one day, {userName} was walking through the woods while wearing a {favColor} t-shirt. {userName} saw a {favAnimal} listening to {favBand}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Madlib();
         }
     }
 }
